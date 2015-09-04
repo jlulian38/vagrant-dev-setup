ea14db017cffbf5497d883c4a947760ec5b03c75
Julian R. Moore <jreedmoore@gmail.com>
2015-09-03 22:08:46 -0400

    Initial vagrant setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get -y install i3 open-vm-tools-dkms xserver-xorg xdm git terminator emacs
+
+sudo -u vagrant bash -c 'git clone https://github.com/svetlyak40wt/dotfiler.git .dotfiles'
+
+sudo -u vagrant bash -c 'git clone https://github.com/jlulian38/dot-userland.git .dotfiles/userland'
+sudo -u vagrant bash -c 'git clone https://github.com/jlulian38/dot-emacs.git .dotfiles/emacs'
+
+sudo -u vagrant bash -c 'rm ~/.bashrc; export PATH=~/.dotfiles/bin/:$PATH; dot update'
+
+service xdm restart
