e795632f8b18f9d799d7ec144869c41fda5add49
Vagrant User <vagrant@vagrant.vm>
2015-09-04 13:56:10 +0000

    Add LaTeX and evince (the PDF viewer)

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 apt-get update
 
-apt-get -y install i3 open-vm-tools-dkms xserver-xorg xdm git terminator emacs
+apt-get -y install i3 open-vm-tools-dkms xserver-xorg xdm git terminator emacs \
+        texlive-latex-recommended evince
 
 sudo -u vagrant bash -c 'git clone https://github.com/svetlyak40wt/dotfiler.git .dotfiles'
 
